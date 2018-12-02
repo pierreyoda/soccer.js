6babfbbe025c57b2012d2e886018ad2a9479d11f
pierreyoda <pierreyoda@users.noreply.github.com>
2018-12-02 20:44:18 +0100

    :speech_balloon: About page content

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>About Soccer.js</h1>
+    <p>
+      Soccer.js is an open-source clone of the realtime multiplayer
+      game <a href="https://www.haxball.com">Haxball</a>.
+    </p>
+    <h2>Technology Stack</h2>
+    <p>
+      Soccer.js is composed of two separate Typescript application sharing some common code.
+    </p>
+    <h3>Client</h3>
+    <p>
+      The Soccer.js web client uses Vue.js with Tailwind CSS for styling.
+    </p>
+    <h3>Server</h3>
+    <p>
+      The Soccer.js server uses an Express-based Node.js stack built on Feathers.
+      Communication with the clients is achieved with socket.io.
+    </p>
   </div>
 </template>
 
