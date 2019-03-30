45e84f24d90ebc4fe517379b4f8a8d1a12775df8
pierreyoda <pierreyoda@users.noreply.github.com>
2019-03-30 23:59:58 +0100

    :wrench: Temporarily disable PurgeCSS for the frontend

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,11 +2,11 @@ module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        "./public/**/*.html",
-        "./src/**/*.vue",
-      ],
-    }),
+    // require('@fullhuman/postcss-purgecss')({
+    //   content: [
+    //     "./public/**/*.html",
+    //     "./src/**/*.vue",
+    //   ],
+    // }),
   ],
 };
