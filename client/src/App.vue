6bd6034635fd1cf9a2de71b80d0021e63d8e197f
pierreyoda <pierreyoda@users.noreply.github.com>
2019-03-31 18:37:45 +0200

    :lipstick: Pick colors, font & start redesign of navbar & home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,61 @@
 <template>
-  <div id="app" class="text-center">
-    <div class="bg-black">
-      <span class="bg-blue-700 font-semibold">
-        <router-link to="/" class="no-underline text-white">Soccer.js</router-link>
-      </span>
-      <router-link to="/play" class="no-underline text-white">Play</router-link>
-      <router-link to="/about" class="no-underline text-white">About</router-link>
+  <div id="app" class="flex flex-col items-center">
+    <nav class="w-full h-full flex flex-row items-center justify-between">
+      <h1 class="brand">
+        Soccer.js
+      </h1>
+      <div class="flex flex-row items-center justify-center">
+        <router-link to="/" class="nav-link">Home</router-link>
+        <router-link to="/play" class="nav-link">Play</router-link>
+        <router-link to="/about" class="nav-link">About</router-link>
+      </div>
+      <a target="_blank" href="https://github.com/pierreyoda/soccer.js" class="github-link">
+        <img src="./assets/github.png" alt="Github">
+      </a>
+    </nav>
+    <div class="flex flex-col items-center justify-start content">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 @import "app";
 
-a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: 'Montserrat', 'Roboto', sans-serif;
+}
+
+#app {
+  @apply min-h-screen text-center;
+}
+
+nav {
+  @apply bg-sjs-teal-600 py-2 px-8;
+  .brand {
+    @apply font-bold text-2xl text-white;
+  }
+  .nav-link {
+    @apply no-underline font-bold text-lg text-white mr-6;
+    &:last-child {
+      @apply mr-0;
+    }
+    &:hover {
+      @apply text-sjs-blue-grey-300;
+    }
+    &.router-link-exact-active {
+      @apply text-sjs-blue-100;
+    }
+  }
+  .github-link {
+    img {
+      width: 30px;
+      height: auto;
+    }
+  }
+}
+
+.content {
+  @apply w-full h-full bg-sjs-blue-grey-050;
 }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,107 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Soccer.js</h1>
-  </div>
+  <section class="flex flex-col md:flex-row hero">
+    <div class="w-full md:w-1/2 flex flex-col items-center p-12">
+      <div class="hero-img-container">
+        <img alt="Vue logo" src="../assets/logo.png" class="hero-img">
+      </div>
+      <router-link to="/play"
+        class="bg-sjs-teal-500 hover:bg-sjs-teal-700 py-2 px-4 rounded font-bold text-xl text-white">
+        Play now!
+      </router-link>
+    </div>
+    <div class="w-full md:w-1/2 flex flex-col items-start py-8 px-8 text-left">
+      <h2 class="font-bold text-sjs-teal-700 text-5xl mb-3">
+        Soccer.js
+      </h2>
+      <p class="font-semibold text-sjs-teal-700 mb-12">
+        Soccer.js is a free, open-source multiplayer top-down football-like game
+        based on physics and teamplay.<br><br>
+        Open-source clone of Haxball.
+      </p>
+      <div class="flex flex-col items-start">
+        <div v-for="(feature, i) in features" :key="i"
+          class="flex flex-row items-center justify-start hero-feature">
+          <div class="flex flex-col items-center justify-center icon-container">
+            <img class="icon" :src="feature.iconImg" :alt="feature.title">
+          </div>
+          <div class="flex flex-col items-start text-left">
+            <h4 class="font-bold text-sjs-blue-grey-800">
+              {{ feature.title }}
+            </h4>
+            <p class="font-semibold text-sm text-sjs-blue-grey-900">
+              {{ feature.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+interface Feature {
+  iconImg: string;
+  title: string;
+  description: string;
+}
+
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  readonly features: Feature[] = [
+    {
+      iconImg: require("../assets/icons/icon-server.svg"),
+      title: "Authoritative server",
+      description: "A centralized server architecture for lag compensation and anti-cheat protection.",
+    },
+    {
+      iconImg: require("../assets/icons/icon-chart.svg"),
+      title: "Statistics",
+      description: "Live statistics on the match in progress.",
+    },
+    {
+      iconImg: require("../assets/icons/icon-user-circle.svg"),
+      title: "Accounts",
+      description: "An optional, free player accounts system.",
+    },
+    {
+      iconImg: require("../assets/icons/icon-code.svg"),
+      title: "Open-source",
+      description: "A fully open-source code under the MIT license. Contributions are welcome!",
+    },
+  ];
+}
 </script>
+
+<style lang="scss" scoped>
+.hero-img-container {
+  width: auto;
+  height: 400px;
+  @screen md {
+    height: 500px;
+  }
+  img {
+    width: auto;
+    height: 100%;
+    max-width: 100%;
+  }
+}
+
+.hero-feature {
+  @apply mb-8;
+  &:last-child {
+    @apply mb-0;
+  }
+  .icon-container {
+    @apply rounded-full bg-sjs-blue-grey-200 mr-6;
+    $size: 50px;
+    width: $size;
+    height: $size;
+    .icon {
+      width: $size / 2;
+      height: $size / 2;
+    }
+  }
+}
+</style>
