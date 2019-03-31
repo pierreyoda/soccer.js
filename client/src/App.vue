698e90d12842c5588a421499f038a6a031fe2eb9
pierreyoda <pierreyoda@users.noreply.github.com>
2019-03-31 16:17:44 +0200

    :arrow_up: Client: upgrade to Tailwind 1.0 beta

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="text-center">
     <div class="bg-black">
-      <span class="bg-blue-darker font-semibold">
+      <span class="bg-blue-700 font-semibold">
         <router-link to="/" class="no-underline text-white">Soccer.js</router-link>
       </span>
       <router-link to="/play" class="no-underline text-white">Play</router-link>
